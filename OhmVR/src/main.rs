3c4ee504080064027814d9ac993d399a2e52831a
suspiciouslyBee <suspiciously@bee.movie>
2024-04-22 10:48:37 -0700

    + Basic structure drafted

--- OhmVR/src/main.rs
+++ OhmVR/src/main.rs
@@ -1,3 +1,10 @@
+pub mod framework;
+
+
+/*
+This code should probably be unit tests until something works. */
+
 fn main() {
     println!("Hello, world!");
+
 }
--- OhmVR/target/debug/deps/OhmVR-bb8858e9f978596d.d
+++ OhmVR/target/debug/deps/OhmVR-bb8858e9f978596d.d
@@ -1,5 +1,8 @@
-/home/suspiciouslybee/repos/OhmVR/OhmVR/target/debug/deps/libOhmVR-bb8858e9f978596d.rmeta: src/main.rs
+/home/suspiciouslybee/repos/OhmVR/OhmVR/target/debug/deps/libOhmVR-bb8858e9f978596d.rmeta: src/main.rs src/framework.rs src/framework/backend.rs src/framework/data_model.rs
 
-/home/suspiciouslybee/repos/OhmVR/OhmVR/target/debug/deps/OhmVR-bb8858e9f978596d.d: src/main.rs
+/home/suspiciouslybee/repos/OhmVR/OhmVR/target/debug/deps/OhmVR-bb8858e9f978596d.d: src/main.rs src/framework.rs src/framework/backend.rs src/framework/data_model.rs
 
 src/main.rs:
+src/framework.rs:
+src/framework/backend.rs:
+src/framework/data_model.rs:
