54c14cbdea7163fb5183abdc7174a19e09c06e1c
suspiciouslyBee <suspiciously@bee.movie>
2024-04-22 10:55:08 -0700

    im smart i forgot to ignore target

--- OhmVR/src/framework.rs
+++ OhmVR/src/framework.rs
@@ -1,2 +1,6 @@
 mod backend;
 mod data_model;
+
+/*
+The Ohm framework is essentiall
+*/
--- OhmVR/src/main.rs
+++ OhmVR/src/main.rs
@@ -4,6 +4,7 @@ pub mod framework;
 /*
 This code should probably be unit tests until something works. */
 
+
 fn main() {
     println!("Hello, world!");
 
